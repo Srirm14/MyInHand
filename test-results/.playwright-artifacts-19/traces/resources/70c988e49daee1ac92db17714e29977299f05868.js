51954ed38c5be28194acc51fba57da2e72a17391
Sriram.Leo <sriram.leo@deltaww.com>
2026-04-05 20:16:41 +0530

    fixes

--- test-results/.playwright-artifacts-19/traces/resources/70c988e49daee1ac92db17714e29977299f05868.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/[root-of-the-server]__0pvklx7._.css","static/chunks/src_0q4oiox._.js","static/chunks/node_modules_next_0wqlw6r._.js","static/chunks/node_modules_@base-ui_react_esm_0zuvs5f._.js","static/chunks/node_modules_tailwind-merge_dist_bundle-mjs_mjs_0ezz.e5._.js","static/chunks/node_modules_@supabase_auth-js_dist_module_0.pzobh._.js","static/chunks/node_modules_@tanstack_query-core_build_modern_0wpyu7q._.js","static/chunks/node_modules_0da8_ct._.js"],
-    source: "dynamic"
-});
